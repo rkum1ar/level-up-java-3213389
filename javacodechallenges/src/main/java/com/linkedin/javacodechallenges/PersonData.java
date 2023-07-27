a03c4c183bd5d4a6db250eeda1434dc5748b2e77
rkum1ar <139589724+rkum1ar@users.noreply.github.com>
2023-07-27 13:41:24 +0000

    submit

---
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/PersonData.java
@@ -0,0 +1,14 @@
+package com.linkedin.javacodechallenges;
+
+public class PersonData {
+  private String firstName;
+  private String lastName;
+  int age;
+
+public PersonData(String firstName, String lastName, int age ){
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.age = age;
+}
+
+}
