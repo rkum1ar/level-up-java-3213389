df680ddf7b3ada276eb8bf52dcea39f6df470f22
rkum1ar <139589724+rkum1ar@users.noreply.github.com>
2023-07-27 13:41:37 +0000

    submit

---
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/Person.java
@@ -0,0 +1,8 @@
+package com.linkedin.javacodechallenges;
+
+public class Person {
+  public static void main(String[] args){
+    PersonData p1 = new PersonData("Rajesh", " Kumar" , -24);
+    p1.introdueYouself(); 
+  }
+}
--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/PersonData.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/PersonData.java
@@ -10,5 +10,32 @@ public PersonData(String firstName, String lastName, int age ){
   this.lastName = lastName;
   this.age = age;
 }
+public int getAge(){
+  return this.age;
+}
 
+public void setAge(int age){
+  if (age < this.age) {
+    System.out.println("You can't make a person younger !" +
+        "not set");
+  }else {
+    this.age =age;
+  }
+  }
+public String getFirstName(){
+return firstName;
+}
+public void setFristName(String firstName){
+  this.firstName = firstName;
+}
+public String getLastName(){
+  return lastName;
+}
+public void setLastName(String lastName){
+this.lastName = lastName;
+}
+public void introdueYouself(){
+  System.out.println("Hi, my name is " + getFirstName() + 
+  getLastName() + " " + "and I'm " + getAge() + " year old");
+}
 }
